5fcf49ffaf3583e0fd6dcae255ac992b982d8bb4
PauladO <pauladenouden@gmail.com>
2017-03-19 13:32:14 +0100

    Install cloudify and image uploader, created artist index and show, added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.delete_all
+Artist.delete_all
+
+mj = Artist.create(name: "Michael Jackson", remote_image_url: "http://res.cloudinary.com/doei2x1xz/image/upload/v1489920468/ziqzw8fmjkiohesxgcbj.jpg")
+kdrie = Artist.create(name: "K3", remote_image_url:
+"http://res.cloudinary.com/doei2x1xz/image/upload/v1489920468/io7fdjzusw1pzds33edc.jpg")
+rick = Artist.create(name: "Ricky A", remote_image_url: "http://res.cloudinary.com/doei2x1xz/image/upload/v1489920469/yl1xropv2efrymthyv2z.gif")
+
+roll = Song.create(name: "Never gonna give you up", artist: rick)
+allekleuren = Song.create(name: "Alle kleuren", artist: kdrie)
+omas = Song.create(name: "Oma's aan de top", artist: kdrie)
+blub = Song.create(name: "Blub, ik ben een vis", artist: kdrie)
+toveren = Song.create(name: "Toveren", artist: kdrie)
+thriller = Song.create(name: "Thriller", artist: mj)
+billy = Song.create(name: "Billy Jean", artist: mj)
